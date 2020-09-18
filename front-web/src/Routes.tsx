86c41056d3074bdcf7c6dce5149bbfe9143b7bac
Matheus Ferreira <matheus_fsilva05@hotmail.com>
2020-09-17 21:20:54 -0300

    Adicionando a página de gráficos

--- front-web/src/Routes.tsx
+++ front-web/src/Routes.tsx
@@ -1,8 +1,9 @@
 import React from 'react';
-import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import { BrowserRouter, Route, Switch } from "react-router-dom";
 import Header from './components/Header';
 import Home from './pages/Home';
 import Records from './pages/Records';
+import Charts from './pages/Charts';
 
 const Routes = () => (
     <BrowserRouter>
@@ -11,11 +12,14 @@ const Routes = () => (
             <Route path="/" exact>
                 <Home />
             </Route>
-            <Route path="/records"> 
-                <Records />
+            <Route path="/records">
+              <Records />
+            </Route>
+            <Route path="/charts">
+              <Charts />
             </Route>
         </Switch>
     </BrowserRouter>
 );
 
-export default Routes;
+export default Routes
--- front-web/src/pages/Records/index.tsx
+++ front-web/src/pages/Records/index.tsx
@@ -1,39 +1,33 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useState } from "react";
+import "./styles.css";
 import axios from 'axios';
-import './styles.css';
-import { RecordsResponse } from './types';
-import { formatDate } from './helpers';
+import { RecordsResponse } from "./types";
+import {formatDate} from './helpers'
 import Pagination from './Pagination';
-import { Link } from 'react-router-dom';
+import Filters from '../../components/Filters'
 
 const BASE_URL = 'http://localhost:8080'
 
 const Records = () => {
-    const [ recordsResponse, setRecordsResponse] = useState<RecordsResponse>();
+    const [recordsResponse, setRecordsResponse] = useState<RecordsResponse>();
     const [activePage, setActivePage] = useState(0);
 
     useEffect(() => {
-       axios.get(`${BASE_URL}/records?linesPerPage=12&page=${activePage}`)
-       .then(response => setRecordsResponse(response.data));
+        axios.get(`${BASE_URL}/records?linesPerPage=12&page=${activePage}`)
+            .then(response => setRecordsResponse(response.data))
     }, [activePage]);
 
     const handlePageChange = (index: number) => {
-        setActivePage(index)
+        setActivePage(index);
     }
 
     return (
         <div className="page-container">
-            <div className="filters-container records-actions">
-                <Link to="/charts">
-                    <button className="action-filters">
-                        VER GRÁFICO
-                    </button>
-                </Link>
-            </div>
-
+            <Filters link="/charts" linkText="VER GRÁFICO" />
+            
             <table className="records-table" cellPadding="0" cellSpacing="0">
                 <thead>
-                    <tr>
+                    <tr >
                         <th>INSTANTE</th>
                         <th>NOME</th>
                         <th>IDADE</th>
@@ -44,25 +38,26 @@ const Records = () => {
                 </thead>
                 <tbody>
                     {recordsResponse?.content.map(record => (
-                          <tr key={record.id}>
+                        <tr key={record.id}>
                             <td>{formatDate(record.moment)}</td>
-                            <td>{record.name}</td>
+                            <td>{record.name} </td>
                             <td>{record.age}</td>
                             <td className="text-secondary">{record.gamePlatform}</td>
                             <td>{record.genreName}</td>
-                            <td className="text-primary">{record.gameTitle}</td>
-                      </tr> 
-                    ))};                     
-                            
+                            <td className="text-primary">{record.gameTitle}</td>                            
+                        </tr>
+                    ))}
+
                 </tbody>
             </table>
             <Pagination
              activePage={activePage}
              goToPage={handlePageChange}
              totalPages={recordsResponse?.totalPages}
+
              />
         </div>
-    );   
+    );
 }
 
 export default Records;
